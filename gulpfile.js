adccb83566daf86cedb54db78fe67c95554b2480
Brian Emilius <be@RTS.dk>
2017-03-22 13:34:06 +0100

    gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+// include gulp
+var gulp   = require('gulp'),
+
+// include plugins
+    pug    = require('gulp-pug'),
+    inject = require('gulp-inject');
+
+// default gulp task
+gulp.task('default', [], () => {
+    console.log("YOOOOOO");
+});
