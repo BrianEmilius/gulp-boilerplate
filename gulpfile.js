deebd832ab2f77d582caac906748aafe4682d2fe
Brian Emilius <brian@brianemilius.com>
2017-03-22 16:20:01 +0100

    added lots of packages and sass structure

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,25 @@
 // include gulp
-var gulp   = require('gulp'),
+var gulp = require('gulp'),        // duh?
 
 // include plugins
-    pug    = require('gulp-pug'),
-    inject = require('gulp-inject');
+	pug    = require('gulp-pug'),    // HTML templating
+	inject = require('gulp-inject'), // inject scripts and css in HTML documents
+	prompt = require('gulp-prompt'); // add interactions to gulp tasks
+
+// constants
+const DEV_DIR  = './dev/',  // development directory path
+			DIST_DIR = './dist/'; // distribution directory path
 
 // default gulp task
 gulp.task('default', [], () => {
-    console.log("YOOOOOO");
+	gulp.src(DEV_DIR + 'assets/javascripts/test.js')
+		.pipe(prompt.prompt({
+			type: 'checkbox',
+			name: 'packages',
+			message: 'Which packages would you like to build with?',
+			choices: ['Materialize/Angular1', 'Bootstrap/jQuery']
+		}, function(res){
+			//value is in res.packages (as an array) 
+			console.log(res.packages);
+		}));
 });
